b735a1b340ec794804ab873a8e0069b768bf730b
CreadivSol <business@creadivsol.com>
2025-12-23 08:46:19 +0500

    Update AllowedOrigins.js

--- utils/AllowedOrigins.js
+++ utils/AllowedOrigins.js
@@ -1,5 +1,1 @@
-export const allowedOrigins = [
-    "http://localhost:3000",
-    "https://mkzdarood.vercel.app",
-    undefined 
-]
+export const allowedOrigins = ["http://localhost:3000", "https://www.creadivsol.com", "http://192.168.0.102:3000"];
