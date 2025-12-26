d19a3d4e810f051817acc8c396d7c9ddc2175247
wasif903 <wasifmehmood903@gmail.com>
2025-12-27 04:55:52 +0500

    Updated Changes

--- utils/NodeMailerConfig.js
+++ utils/NodeMailerConfig.js
@@ -1,12 +1,15 @@
 import nodemailer from 'nodemailer'
+import dotenv from "dotenv";
+
+dotenv.config();
 
 let transporter = nodemailer.createTransport({
     host: 'smtp.hostinger.com',
     port: 465,
     secure: true,
     auth: {
-        user: 'business@creadivsol.com',
-        pass: 'CreadivSol@001'
+        user: process.env.EMAIL,
+        pass: process.env.PASS
     },
     tls: { rejectUnauthorized: false }
 });
